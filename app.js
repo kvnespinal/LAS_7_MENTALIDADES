08082cc1f35c28a7262a2a9b3606687dd1779df8
kvnespinal <espinalkevin98@gmail.com>
2026-08-15 23:31:07 -0600

    TEST_TITLE_FROM_DESKTOP

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ function showWelcomeScreen() {
 
             <h1>
                 LAS 7<br>
-                MENTALIDADES
+                MENTALIDADES PFKR.I.
                 
             </h1>
 
